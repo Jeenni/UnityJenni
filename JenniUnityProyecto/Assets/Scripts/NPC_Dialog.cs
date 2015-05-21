21cc1aecfda8e47bb9c42e2d2b1f8ddb2d331e74
Jeenni <jennifer_gonzalez_2@hotmail.com>
2015-05-21 18:44:32 +0200

    NPC va - Murallita

--- JenniUnityProyecto/Assets/Scripts/NPC_Dialog.cs
+++ JenniUnityProyecto/Assets/Scripts/NPC_Dialog.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class NPC_Dialog : MonoBehaviour {
-
+	
 	public string[] answerButtons;
 	public string[] Questions;
 	bool DisplayDailog = false;
@@ -10,56 +10,56 @@ public class NPC_Dialog : MonoBehaviour {
 	bool ActivateQuestSiesqueno = false;
 	bool Activa3 = false;
 	bool Activa4 = false;
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	void OnGUI(){
 		GUILayout.BeginArea (new Rect (700, 600, 400, 400));
-
+		
 		if (DisplayDailog && !a && !ActivateQuestSiesqueno && !Activa3) {
-
+			
 			//Puedes ayudarme
 			GUILayout.Label(Questions[0]);
-
-
+			
+			
 			if(GUILayout.Button(answerButtons[0])){//si
 				a = true;
-
+				
 				//DisplayDailog = false;
 				ActivateQuestSiesqueno=false;
 			}
 			if(GUILayout.Button(answerButtons[1])){//No
-
+				
 				ActivateQuestSiesqueno=true;
-
+				
 				//DisplayDailog= false;
 				a = false;
-
-
+				
+				
 			}
 		}
-
+		
 		//Si es que no
 		if (DisplayDailog && ActivateQuestSiesqueno) {
 			GUILayout.Label(Questions[3]);
-
+			
 			if(GUILayout.Button(answerButtons[4])){
 				DisplayDailog= false;
 				ActivateQuestSiesqueno=false;
 			}
-
-
-
+			
+			
+			
 		}
-
+		
 		//si es que si
 		if (DisplayDailog && a && !Activa3 && !Activa4) {
 			GUILayout.Label(Questions[1]);
@@ -91,16 +91,16 @@ public class NPC_Dialog : MonoBehaviour {
 		//Hay un mecanism
 		if (DisplayDailog && Activa3) {
 			GUILayout.Label(Questions[2]);
-		
+			
 		}
 		GUILayout.EndArea ();
 		
-
+		
 	}
-
+	
 	void OnTriggerEnter(){
 		DisplayDailog = true;
-
+		
 	}
 	void ObnTriggerExit(){
 		DisplayDailog = false;
